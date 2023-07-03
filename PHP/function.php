0e706d16d79c113e706eb7b2ea272facfe60a3da
GeniusJoyTech <geniusjoytech@gmail.com>
2023-07-02 21:30:18 -0300

    Novo commit

---
+++ PHP/function.php
@@ -0,0 +1,4 @@
+<?php
+function exemploPhp(){
+    echo 'hello world';
+}
--- PHP/imprimindoTexto.php
+++ PHP/imprimindoTexto.php
@@ -2,7 +2,7 @@
 // PHP é case sensitive, logo diferencia letras maiusculas de minusculas.
 define("CONSTANTE", "Olá Mundo.");
 echo CONSTANTE; // imprime "Olá Mundo."
-echo Constante; // Lança um erro Error: Undefined constant "Constante"
+//echo Constante; // Lança um erro Error: Undefined constant "Constante"
 echo "Olá a todos." 
 
 ?>
